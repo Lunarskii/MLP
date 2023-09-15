46db645b1d47c02772366c34742d5ecde8f9e5ba
Lunarskii <lunarskii0@gmail.com>
2023-09-15 22:51:01 +0300

    moved

--- src/metrics_widget/main.cc
+++
@@ -1,9 +0,0 @@
-#include "metrics_widget.h"
-#include <QApplication>
-
-int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    s21::MetricsWidget mw;
-    mw.show();
-    return a.exec();
-}
